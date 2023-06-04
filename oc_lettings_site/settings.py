ac90b7cf57ee179c899c8d3ee98cb52102f75b06
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-06-04 22:07:45 +0200

    update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['lettingssite.azurewebsites.net']
+ALLOWED_HOSTS = ['p8wmtch6es.eu-west-1.awsapprunner.com']
 
 # Application definition
 
