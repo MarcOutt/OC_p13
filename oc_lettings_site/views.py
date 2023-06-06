882d45e15f1b613cb0f9cab3cd6fd84a0eb3283e
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-06-06 13:58:26 +0200

    update views file of oc_lettingsÃ_site

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -5,6 +5,7 @@ def trigger_error(request):
     division_by_zero = 1 / 0
     return division_by_zero
 
+
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo
 # consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis,
 # sem mi convallis eros, vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum,
