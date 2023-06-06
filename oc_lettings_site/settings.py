a2070425daf227074125c65d706bebb984a22ed9
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-06-06 12:45:33 +0200

    update requirements.txt

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,8 +1,7 @@
 import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-from decouple import Config
-
+from decouple import Config, RepositoryEnv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
