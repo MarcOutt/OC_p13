2e1ab96c2a49e892daa01120ee1c0d86501c904b
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-06-08 20:08:17 +0200

    update css admin

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -134,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+ADMIN_CSS = 'css/admin.css'
+ADMIN_LOGO = 'images/logo.png'
+
