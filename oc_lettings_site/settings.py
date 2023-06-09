b4ac6de5ea28d9fabdfdc4f1e1d921e2c892e798
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-06-09 10:10:49 +0200

    update templates and settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -134,7 +134,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-ADMIN_CSS = 'css/admin.css'
-ADMIN_LOGO = 'images/logo.png'
-
