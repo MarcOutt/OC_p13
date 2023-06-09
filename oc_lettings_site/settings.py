578cc737a8156aae506cbc537a361269c7a91e6c
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-06-09 13:16:32 +0200

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://p8wmtch6es.eu-west-1.awsapprunner.com/']
+ALLOWED_HOSTS = ['https://p8wmtch6es.eu-west-1.awsapprunner.com']
 
 # Application definition
 
