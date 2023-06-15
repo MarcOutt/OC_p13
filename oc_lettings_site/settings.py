b58556cb3b8dce9768f1ba895f0304e6aa7c40f1
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-06-15 18:17:14 +0200

    update settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -12,12 +12,12 @@ config = Config(RepositoryEnv(BASE_DIR + '/.env'))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS=['3.252.160.237', '0.0.0.0', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['3.252.160.237', '0.0.0.0', '127.0.0.1', 'localhost']
 
 # Application definition
 
