1936e10e241ffd99665f397345f049ed8b1f9e73
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-06-15 14:46:39 +0200

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '3.252.160.237']
+ALLOWED_HOSTS = ['3.252.160.237', '0.0.0.0', '127.0.0.1', 'localhost']
 
 # Application definition
 
