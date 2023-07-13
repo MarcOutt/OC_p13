c627201eeae57879bfc3922d7dd97c4d3a40622f
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-07-13 19:35:18 +0200

    update config.yml

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -69,23 +69,22 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
-if '--no-sentry' in sys.argv:
-    if os.environ.get('SENTRY_DSN'):
-        sentry_sdk.init(
-            dsn=os.environ.get('SENTRY_DSN'),
-            integrations=[
-                DjangoIntegration(),
-            ],
+if os.environ.get('SENTRY_DSN') != '':
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_DSN'),
+        integrations=[
+            DjangoIntegration(),
+        ],
 
-            # Set traces_sample_rate to 1.0 to capture 100%
-            # of transactions for performance monitoring.
-            # We recommend adjusting this value in production.
-            traces_sample_rate=1.0,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
 
-            # If you wish to associate users to errors (assuming you are using
-            # django.contrib.auth) you may enable sending PII data.
-            send_default_pii=True
-        )
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
