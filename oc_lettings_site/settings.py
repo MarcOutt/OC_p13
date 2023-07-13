b2085fa2c10acbeadc183be09215adf1d12fcea4
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-07-13 19:21:37 +0200

    update config.yml

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,7 +1,5 @@
 # sourcery skip: use-fstring-for-concatenation
 import os
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 
 import config
 
@@ -67,22 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
-if os.environ.get('SENTRY_DSN'):
-    sentry_sdk.init(
-        dsn=os.environ.get('SENTRY_DSN'),
-        integrations=[
-            DjangoIntegration(),
-        ],
-
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
-
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True
-    )
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
