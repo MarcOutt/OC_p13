901d3156a6fcd9c9d26bcd02ff0f5c4989a286b1
MarcOutt <112987151+MarcOutt@users.noreply.github.com>
2023-07-16 13:59:10 +0200

    update config.yml

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-SENTRY_DSN = "https://49eaf6eddc7548339774b2a1677d9b7d@o4505311407243264.ingest.sentry.io/4505311422709760"
+SENTRY_DSN = "https://49eaf6eddc7548339774b2a1677d9b7d@o4505311407243264." \
+             "ingest.sentry.io/4505311422709760"
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
